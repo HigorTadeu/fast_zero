924bdc7cdfcdc8c3365e7154c74f48ae84a8c191
Higor Tadeu <higorse@gmail.com>
2024-08-08 22:44:35 -0300

    Primeiro commit

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange (Organização)
+    response = client.get('/')  # Essa função retorna o EndPoint # Act (Ação)
+    assert response.status_code == HTTPStatus.OK  # assert
+    assert response.json() == {'message': 'Olá Mundo!'}
